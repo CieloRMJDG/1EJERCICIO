30ec8320fc757e060c551d7f8377af4f8484758b
Cielorodr <skylbr2712@gmail.com>
2025-09-04 19:28:08 -0500

    Agrego archivo teoria.js con constantes

---
+++ teoria.js
@@ -0,0 +1,18 @@
+//constantes
+
+/**let nombre = "Jefferson";
+
+const saludar = (nombre = `Invitado`) => {
+  //  const apellido = "Perez";
+    console.log(`Hola ${nombre}`);
+   
+}
+ saludar ();
+
+ const a = [1,2,3,4,5];
+ const b= [6,7,8,9,10];
+ const c = [`perro`,`gato`,`pez`];
+
+ console.log(...a,...b,...c);
+ */
+
